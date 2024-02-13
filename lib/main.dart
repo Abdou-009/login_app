53b7a593c1d21b030e9661183f3ee2bdc1f65111
Abdou_009 <brardadz0531@gmail.com>
2024-02-13 15:45:59 +0100

    second commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:logapp/app.dart';
+import 'package:logapp/firebase_options.dart';
+import 'package:logapp/simple_bloc_observer.dart';
+import 'package:user_repository/user_repository.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  Bloc.observer = SimpleBlocObserver();
+  runApp(MyApp(FirebaseUserRepo()));
+}
